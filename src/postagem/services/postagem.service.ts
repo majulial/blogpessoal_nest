3f5de7e8ac417f8f719a87dbe12607c33690e9ce
Júlia de Lima <maria.julial2514@gmail.com>
2026-03-03 13:07:22 -0300

    ✨ New Feature: Cronstrução do recurso postagem

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { Postagem } from "../entities/postagem.entity";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { ILike, Repository } from "typeorm";
+import { DeleteResult } from "typeorm/browser";
 
 
 @Injectable()
@@ -17,4 +18,52 @@ export class PostagemService {
         //SELECT * FROM tb_postagens
         return this.postagemRepository.find();
     }
+
+    async findById(id: number): Promise<Postagem>{
+        // SELECT * FROM tb_postagens WHERE id = o que o usuario digitar;
+        const postagem = await this.postagemRepository.findOne({
+            where:{
+                id
+            }
+         })
+
+         if (!postagem)
+            throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+
+         return postagem;
+
+}
+
+        async findAllByTitulo(titulo: string): Promise<Postagem[]>{
+
+            // SELECT * FROM tb_postagens   WHERE titulo LIKE '%?%';
+            return this.postagemRepository.find({ // Usando ILike para ignorar letras maiúsculas e minúsculas
+                where:{
+                    titulo: ILike(`%${titulo}%`)
+                }
+            })
+         }
+
+         async create(postagem: Postagem): Promise<Postagem>{
+            // INSERT INTO tb_postagens (titulo, texto) VALUES (valores digitados pelo usuario);
+            return await this.postagemRepository.save(postagem);
+}        
+
+    async update(postagem: Postagem): Promise<Postagem>{
+
+            if (!postagem.id || postagem.id <= 0)
+                throw new HttpException("Id inválido!", HttpStatus.BAD_REQUEST);
+            await this.findById(postagem.id);
+            // UPDATE tb_postagens SET titulo = o que digitar, texto = ?, data = CURRENT_TIMESTAMP() WHERE id = que colocar;
+            return this.postagemRepository.save(postagem);
+}  
+
+
+    async delete(id: number): Promise<DeleteResult>{
+        await this.findById(id);
+        // DELETE tb_postagens FROM id = ?;
+        return this.postagemRepository.delete(id);
+    }
+
+
 }
