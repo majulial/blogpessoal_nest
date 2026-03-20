4436096b503529052afd4ebdd8d5b76e657c64d4
code seven <https://github.com/codeseven777>
2026-03-19 21:56:39 -0300

    Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/services/postagem.module';
 import { TemaModule } from '../tema/tema.module';
-import { Tema } from '../tema/entities/tema.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
+import { Tema } from '../tema/entities/tema.entity';
+import { Postagem } from './postagem/entities/postagem.entity';
+import { Usuario } from './usuario/entities/usuario.entity';
 
 
 
@@ -29,7 +29,7 @@ TypeOrmModule.forRootAsync({
     //   database: 'db_blogpessoal',
     //   entities:[Postagem, Tema, Usuario],
     //   synchronize: true,
-      //logging: true, // EXIBE O CODIGO SQL NO CONSOLE DO VSCODE // USAR APENAS EM DESENVOLVIMENTO
+    //   logging: true, // EXIBE O CODIGO SQL NO CONSOLE DO VSCODE // USAR APENAS EM DESENVOLVIMENTO
     }),
     PostagemModule,
     TemaModule,
